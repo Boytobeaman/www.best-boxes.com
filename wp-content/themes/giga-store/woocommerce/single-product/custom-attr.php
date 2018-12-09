493d8691e2c50da36785d033e256d4c6eb3212de
Your Name <you@example.com>
2018-12-09 13:31:49 +0800

    add foldedheight for folding crates

--- wp-content/themes/giga-store/woocommerce/single-product/custom-attr.php
+++ wp-content/themes/giga-store/woocommerce/single-product/custom-attr.php
@@ -75,6 +75,11 @@ echo '
 	       
 	      </tr>
 	      <tr>
+	      	<td><h4>productfoldedheight</h4></td>
+	      	<td>$productfoldedheight</td>
+	      </tr>
+	       $productfoldedheight
+	      <tr>
 	        <td><h4>Weight</h4></td>
 	        <td class="internal-dimension">
 	        	<span class="kg pull-left value">
