904ce6b538cf904fe6d4031399f4193e6225607c
Your Name <you@example.com>
2018-12-29 11:39:00 +0800

    change render url

--- wp-content/plugins/theme-customisations/custom/custom.js
+++ wp-content/plugins/theme-customisations/custom/custom.js
@@ -46,9 +46,7 @@ jQuery(function($){
 			$("#contactUs").modal();
 		}
 	})
-	$("#contactUs").off().on('show.bs.modal', function () {
-        var url = window.document.location.href;
-        $('#productURL').val(url);
-    });
+	var url = window.document.location.href;
+  $('#productURL').val(url);
 
 })
